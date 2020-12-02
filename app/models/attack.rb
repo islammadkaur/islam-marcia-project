352a932a9d85e24d1cdbe8c8aac23af332f50e7d
Marcia-Free <MarciaFreeman2011@gmail.com>
2020-12-02 13:36:56 -0600

    Associations done

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,2 +1,6 @@
 class Attack < ApplicationRecord
+    has_many :battles
+    has_many :players, through: :battles
+    has_many :enemies, through: :battles
+    
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+    has_many :battles
+    has_many :players, through: :battles
+    has_many :enemies, through: :battles
+    
+
 end
