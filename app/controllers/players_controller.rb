761f4558ba161d319d171034009f1b96e9a888fe
Marcia-Free <MarciaFreeman2011@gmail.com>
2020-12-04 16:25:13 -0600

    Attack showing and random selection done

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,32 +1,41 @@
 class PlayersController < ApplicationController
-    def index
+
+      def index
         @players = Player.all
       end
     
+
       def show
         @player = Player.find(params[:id])
-        @attack_slots = @player.attacks
+        attacks
       end
-    
+
+
       def new
         @player = Player.new
-        @attack_slots = @player.attacks << Attack.all.sample(3)
       end
   
+
       def create
         @player = Player.new(player_params.merge(health: 100))
-        if @player.save
+          if @player.save
+            3.times do |attack|
+              random_attack = Attack.all.sample
+              PlayerAttack.create(player_id: @player.id, attack_id: random_attack.id)
+          end
           redirect_to player_path(@player)
         else
           render :new
         end
       end
   
+
       def edit
         @player = Player.find(params[:id])
-        @attack_slots = @player.attacks
+        attacks
       end
     
+
       def update
           @player = Player.find(params[:id])
           if @player.update(player_params)
@@ -36,16 +45,31 @@ class PlayersController < ApplicationController
           end
       end
     
-    
+
       def destroy
         @player = Player.find(params[:id])
         if @player.destroy
           redirect_to players_path
         end
-  
       end
 
+
+      def attacks
+        @player_attacks = PlayerAttack.where(player_id: @player.id)
+        @attack_slots = []
+
+        Attack.all.select do |attack|
+          @player_attacks.select do |player_attack|
+              if attack.id === player_attack.attack_id 
+                @attack_slots << attack
+              end
+            end 
+        end 
+        @attack_slots
+    end
      
+    
+
       private
   
         def player_params
