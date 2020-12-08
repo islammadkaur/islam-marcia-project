c8b52618a136f090bd0df5318d0e2657f5f2cca2
mixinspace <islammadkaur@gmail.com>
2020-12-08 13:52:36 -0600

    message

--- db/migrate/20201206033809_remove_columns_from_battles.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromBattles < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :battles, :player_attacks_id, :integer
-    remove_column :battles, :attack_id, :integer
-  end
-end
